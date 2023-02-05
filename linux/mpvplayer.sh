96d706d2ec1016871a4f31c76173bcd08cdf5b19
foocaaat <foocaaat@protonmail.com>
2023-02-05 20:59:48 +0200

    20054759

--- linux/mpvplayer.sh
+++ linux/mpvplayer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mpv --alang=jpn,kor --idle --slang=en,eng --no-input-cursor --no-input-default-bindings --no-cache --input-ipc-server=/tmp/mpv-socket --no-config --script-opts=osc-timems=yes --keep-open=yes --keep-open-pause=no --force-window=yes
 
-mpv --alang=jpn,kor --idle --slang=en,eng --no-input-cursor --no-input-default-bindings --no-cache --input-ipc-server=/tmp/mpv-socket --no-config --script-opts=osc-timems=yes --keep-open=yes --keep-open-pause=no
+
