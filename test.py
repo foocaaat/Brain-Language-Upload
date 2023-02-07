82f7b85a605ca8a40a8c907091267c2ee31cc880
foocaaat <foocaaat@protonmail.com>
2023-02-07 11:37:45 +0200

    11074537

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-file=script_dir +"/mpvanki.log"
-file2=script_dir + "/savedata/" + "monke3.er" +  "mpvanki.log"
-print(os.path.abspath(file))
-print(os.path.abspath(file2))
