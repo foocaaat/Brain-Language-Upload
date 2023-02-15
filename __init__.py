c6378d41d86a2e3aa162b29d9d6c7ce483bfb04a
foocaaat <foocaaat@protonmail.com>
2023-02-15 19:06:10 +0200

    19151006

--- __init__.py
+++ __init__.py
@@ -47,6 +47,8 @@ from aqt.import_export.importing import import_file
 import threading
 from datetime import timedelta
 from anki.schedv2 import * 
+import platform
+operating_system = platform.system()
 from aqt.utils import tooltip
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -67,7 +69,6 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 old_fillRev = anki.schedv2.Scheduler._fillRev
 is_old_fillRev = True
-
 
 
 
@@ -101,6 +102,50 @@ def jessycome(object_key):
     # Return the value of the object
     return data.get(object_key)
 
+def  new_fillRev(self, recursing=False) -> bool:
+        "True if a review card can be fetched."
+        if self._revQueue:
+            return True
+        if not self.revCount:
+            return False
+
+        lim = min(self.queueLimit, self._currentRevLimit())
+        if lim:
+            self._revQueue = self.col.db.list(
+                f"""
+select id from cards where
+did in %s and queue = {QUEUE_TYPE_REV} and due <= ?
+order by nid
+limit ?"""
+# order by due, random()
+                % self._deckLimit(),
+                self.today,
+                lim,
+            )
+
+            if self._revQueue:
+                # preserve order
+                self._revQueue.reverse()
+                return True
+
+        return False
+
+
+def created_order_on_off():
+    if is_old_fillRev == True:
+        anki.schedv2.Scheduler._fillRev = new_fillRev
+        is_old_fillRev == False
+    else:
+        anki.schedv2.Scheduler._fillRev = old_fillRev
+        is_old_fillRev == True
+
+created_order_on_off()
+
+if operating_system == "Windows":
+    windows_dir = os.path.join(script_dir, "windows")
+    path = os.environ.get("PATH", "")
+    if windows_dir not in path:
+        os.environ["PATH"] = windows_dir + os.pathsep + path
 
 
 def millis_to_time_format(milliseconds):
@@ -135,16 +180,16 @@ def extract_timestamps(file):
     return timestamps
 
 
-def assemble(*mkve):
-    tempy = os.path.join(str(ankivideo),"tmp.srt") 
+def assemble(mkve, stampa):
+    tempy = os.path.join(str(ankivideo),"stamp/tmp.srt") 
     for file in mkve:
         src_video_paths = []
-        filey = file
+        filey = str(file)
         file = os.path.join(str(ankivideo), file)
         if file.endswith('mkv'):
             src_video_paths.append(file)
         for src_video_path in src_video_paths:
-            tete = os.popen("ffprobe -v error -select_streams s -show_entries stream=index,codec_name:stream_tags=NUMBER_OF_BYTES-eng,NUMBER_OF_BYTES,language -of default=noprint_wrappers=1 -print_format csv " + src_video_path ).read()
+            tete = os.popen("ffprobe -v error -select_streams s -show_entries stream=index,codec_name:stream_tags=NUMBER_OF_BYTES-eng,NUMBER_OF_BYTES,language -of default=noprint_wrappers=1 -print_format csv '" + src_video_path + "'" ).read()
 
 
 
@@ -159,19 +204,21 @@ def assemble(*mkve):
             filtered_lines = [line for line in lines if "ass" in line or "srt" in line or "subrip" in line]
             filty = '\n'.join(filtered_lines)
 
-            filty = '\n'.join(line.split(',')[1] for line in filty.splitlines())
-            tete = int(filty.splitlines()[0])
-
-            os.system(f'mkvextract {src_video_path} tracks {tete}:{tempy}\n')
+            tete = '\n'.join(line.split(',')[1] for line in filty.splitlines())
+            try:
+                tete = int(tete.splitlines()[0])
+            except:
+                global sniff
+                sniff.append(file)
+                return
+            os.system(f'mkvextract "{src_video_path}" tracks {tete}:{tempy}\n')
             subs = pysubs2.load(tempy)
             subs.save(tempy, format="srt")
         timestamps = extract_timestamps(tempy)
         os.remove(tempy)
         counter = 0
 
-        if not os.path.exists(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp"))):
-            os.makedirs(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
-        with open(os.path.join(str(ankivideo), 'stamp/timestamps.txt') , 'a') as f:
+        with open(os.path.join(str(ankivideo), f'stamp/{os.path.splitext(stampa)[0]}.txt') , 'a') as f:
             for start, end in timestamps:
                 counter += 1
                 formatted_counter = str(counter).zfill(5)
@@ -190,108 +237,73 @@ def find_missing_items(list1, list2):
         if item not in list2:
             missing_items.append(item)
     return missing_items
-def get_filenames(folder):
-    exclude_list = jessycome("filelist")
+def synclist():
+    if not os.path.exists(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp"))):
+        os.makedirs(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
+    exclude_list = os.listdir(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
     if not exclude_list:
         exclude_list = []
-    all_files = os.listdir(folder)
-    missing = find_missing_items(exclude_list, all_files)
-    if missing != []:
-        tooltip(f"missing files : {missing}")
-    result = [f for f in all_files if f.endswith('.mkv') and f not in exclude_list]
+    exclude_list = [f for f in exclude_list if f.endswith('.txt')]
+    exclude_list = [os.path.splitext(name)[0] for name in exclude_list]
+
+    all_files = os.listdir(jessycome("ankivideo"))
+    result = [f for f in all_files if (os.path.isdir(os.path.join(ankivideo, f)) or f.endswith('.mkv')) and f != 'stamp']
+    result = [os.path.splitext(name)[0] for name in result]
+    result = [item for item in result if item not in exclude_list]
     result.sort()
     return result
 
-def  new_fillRev(self, recursing=False) -> bool:
-        "True if a review card can be fetched."
-        if self._revQueue:
-            return True
-        if not self.revCount:
-            return False
+def filesinside(folder):
+    all_files = os.listdir(folder)
+    result = [f for f in all_files if f.endswith('.mkv') ]
+    result.sort()
+    return result
 
-        lim = min(self.queueLimit, self._currentRevLimit())
-        if lim:
-            self._revQueue = self.col.db.list(
-                f"""
-select id from cards where
-did in %s and queue = {QUEUE_TYPE_REV} and due <= ?
-order by nid
-limit ?"""
-# order by due, random()
-                % self._deckLimit(),
-                self.today,
-                lim,
-            )
+def ofolder(where):
+    if platform.system() == "Windows":
+        os.startfile(where)
+    else:
+        os.system('xdg-open "%s" &' % os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
+    return
 
-            if self._revQueue:
-                # preserve order
-                self._revQueue.reverse()
-                return True
 
-        return False
-
-import platform
-
-operating_system = platform.system()
-
-try:
+#        ofolder(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
+def addnewstuff():
+    global sniff
+    sniff = []
+    global ankivideo
     ankivideo = jessycome("ankivideo") 
-except:
-    jessygo("ankivideo", f"{script_dir}")
+    if not os.path.exists(os.path.abspath(str(jessycome("ankivideo")))):
+        tooltip("select the folder maan")
+        return
+    be = synclist()
+    time.sleep(2)
+    for thing in be:
+        if os.path.isdir(os.path.abspath(os.path.join(str(jessycome("ankivideo")), thing))):
+            thingss = filesinside(os.path.abspath(os.path.join(str(jessycome("ankivideo")), thing)))
+            thingss = [os.path.join(thing, name) for name in thingss]
+            print(f"folder {thingss}")
+            assemble(thingss, thing)
+            tooltip("doneee")
+        if os.path.isfile(os.path.abspath(os.path.join(str(jessycome("ankivideo")), thing + ".mkv"))):
+            thing = thing + ".mkv"
+            print(f"file {thing}")
+            assemble([thing], thing)
+            tooltip("doneee")
+    if sniff != []:
+        tooltip(f"i sniff a non combatable mkv: {sniff}")
+    ofolder(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
 
 def videofilelocation():
     directory = QFileDialog.getExistingDirectory(None, "Select Directory", "./")
     if not directory:
         return
     jessygo("ankivideo", f"{directory}")
+
+try:
     ankivideo = jessycome("ankivideo") 
-
-def created_order_on_off():
-    if is_old_fillRev == True:
-        anki.schedv2.Scheduler._fillRev = new_fillRev
-        is_old_fillRev == False
-    else:
-        anki.schedv2.Scheduler._fillRev = old_fillRev
-        is_old_fillRev == True
-
-created_order_on_off()
-
-if operating_system == "Windows":
-    windows_dir = os.path.join(script_dir, "windows")
-    path = os.environ.get("PATH", "")
-    if windows_dir not in path:
-        os.environ["PATH"] = windows_dir + os.pathsep + path
-
-def addnewstuff():
-    global ankivideo
-    ankivideo = jessycome("ankivideo") 
-    if not os.path.exists(os.path.abspath(str(jessycome("ankivideo")))):
-        tooltip("select the folder maan")
-        return
-    if os.path.exists(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp/timestamps.txt"))):
-        tooltip("move the timestamp.txt out of the way to make a new one")
-        if platform.system() == "Windows":
-            os.startfile(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
-        else:
-            os.system('xdg-open "%s" &' % os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
-            return
-    be = get_filenames(ankivideo)
-    tooltip("please wait because i didn't make a progress bar")
-    time.sleep(2)
-    if be == []:
-        tooltip("i dont sniff ankything new")
-        return
-    try:
-        assemble(*be)
-        tooltip("doneee")
-        if platform.system() == "Windows":
-            os.startfile(os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
-        else:
-            os.system('xdg-open "%s" &' % os.path.abspath(os.path.join(str(jessycome("ankivideo")), "stamp")))
-            return
-    except:
-        tooltip("i sniff a non combatable mkv")
-        return
+except:
+    tooltip("add the folder maaan")
 
 # create a new menu item, "test"
 action = QAction("On - Off Review Created Order", mw)
@@ -324,8 +336,6 @@ def dueToday(): # formeeeeeeeeeee
         tooltip(dueCount)
 
 
-def answer():
-    Reviewer._answerCard(1)
 
 
 
@@ -448,10 +458,8 @@ def stoopu(when):
 def run_command_field(num=0):
     global ansa
     ansa = 3
-    current_card = mw.reviewer.card
 
-    # Check if the card is new
-    if current_card.queue == 0:
+    if mw.reviewer.card.queue == 0:
         new = "yes"
     else:
         new = "no"
@@ -462,7 +470,6 @@ def run_command_field(num=0):
     sub = "0"
     if num == 1:
         sub = "1"
-        _showHint()
     note = mw.reviewer.card.note()
     # Check if a field called "Command" exists
     if "mpvanki-filename" in note:
@@ -487,7 +494,7 @@ def ansae(ansa):
     mw.update_undo_actions()
 
 
-def add_time(time_string):
+def add_time(time_string, nu):
     # Split the time string into hours, minutes, seconds, and milliseconds
     note = mw.reviewer.card.note()
     time_string2 = note[time_string]
@@ -501,58 +508,44 @@ def add_time(time_string):
     current_time = timedelta(hours=hours, minutes=minutes, seconds=seconds, milliseconds=milliseconds)
 
     # Add 500 milliseconds to the current time
-    new_time = current_time + timedelta(milliseconds=500)
+    if nu == 1: new_time = current_time + timedelta(milliseconds=50)
+    else: new_time = current_time - timedelta(milliseconds=50)
 
     # Format the new time into a string
     new_time_string = "{:01d}.{:02d}.{:02d}.{:03d}".format(new_time.seconds//3600, (new_time.seconds//60)%60, new_time.seconds%60, new_time.microseconds//1000)
 
     note[time_string] = new_time_string
     note.flush()
+
     if time_string == "mpvanki-start":
-        tooltip("-500ms →-----")
+        if nu == 1: tooltip("-100ms →-----")
+        else: tooltip("+100ms ←-----")
+        run_command_field()
     if time_string == "mpvanki-end":
-        tooltip("+500ms -----→")
-    run_command_field()
+        if nu == 1:
+            tooltip("+100ms -----→")
+            new_time2 = current_time - timedelta(milliseconds=150)
+        else:
+            tooltip("-100ms -----←")
+            new_time2 = current_time - timedelta(milliseconds=250)
+        new_time_string2 = "{:01d}.{:02d}.{:02d}.{:03d}".format(new_time2.seconds//3600, (new_time2.seconds//60)%60, new_time2.seconds%60, new_time2.microseconds//1000)
 
+        global mpv
+        try: mpv.command("get_property", "stream-pos")
+        except: mpv = MPV(start_mpv=True, ipc_socket=os.path.abspath("/tmp/mpv-socket"))
 
-def remove_time(time_string):
-    # Get the current note
-    note = mw.reviewer.card.note()
-    # Get the time string from the note
-    time_string2 = note[time_string]
-    # Split the time string into hours, minutes, seconds, and milliseconds
-    time_parts = time_string2.split('.')
-    hours = int(time_parts[0])
-    minutes = int(time_parts[1])
-    seconds = int(time_parts[2])
-    milliseconds = int(time_parts[3])
-
-    # Create a timedelta object with the current time
-    current_time = timedelta(hours=hours, minutes=minutes, seconds=seconds, milliseconds=milliseconds)
-
-    # Remove 500 milliseconds from the current time
-    new_time = current_time - timedelta(milliseconds=500)
-
-    # Format the new time into a string
-    new_time_string = "{:01d}.{:02d}.{:02d}.{:03d}".format(new_time.seconds//3600, (new_time.seconds//60)%60, new_time.seconds%60, new_time.microseconds//1000)
-
-    # Update the note field with the new time
-    note[time_string] = new_time_string
-    note.flush()
-    if time_string == "mpvanki-start":
-        tooltip("+500ms ←-----")
-    if time_string == "mpvanki-end":
-        tooltip("-500ms -----←")
-    run_command_field()
+        if mw.reviewer.card.queue == 0: new = "yes"
+        else: new = "no"
+        mpvankii( note["mpvanki-filename"], new_time_string2, note["mpvanki-end"], note["mpvanki-number"], new, 0)
 
 
 def _addShortcuts(shortcuts):
     """Add shortcuts on Anki 2.1.x"""
     additions = (
-        (SHORTCUT_START1, lambda: remove_time("mpvanki-start")),
-        (SHORTCUT_START2, lambda: add_time("mpvanki-start")),
-        (SHORTCUT_END1, lambda: remove_time("mpvanki-end")),
-        (SHORTCUT_END2, lambda: add_time("mpvanki-end")),
+        (SHORTCUT_START1, lambda: add_time("mpvanki-start", 0)),
+        (SHORTCUT_START2, lambda: add_time("mpvanki-start", 1)),
+        (SHORTCUT_END1, lambda: add_time("mpvanki-end", 0)),
+        (SHORTCUT_END2, lambda: add_time("mpvanki-end", 1)),
         (ANSWER, lambda: ansae(ansa)),
         (HALFSHOW, lambda: run_command_field()),
         (GOBACK, lambda: mw.form.actionUndo.trigger()),
@@ -561,41 +554,10 @@ def _addShortcuts(shortcuts):
     shortcuts += additions
 
 
-def _showHint(incremental=False):
-    """Show hint by activating corresponding links."""
-    mw.web.eval("""
-     var customEvent = document.createEvent('MouseEvents');
-     customEvent.initEvent('click', false, true);
-     var arr = document.getElementsByTagName('a');
-     // Cloze Overlapper support
-     if (typeof olToggle === "function") { 
-         olToggle();
-     }
-     // Image Occlusion Enhanced support
-     var ioBtn = document.getElementById("io-revl-btn");
-     if (!(typeof ioBtn === 'undefined' || !ioBtn)) { 
-         ioBtn.click();
-     }
-     for (var i=0; i<arr.length; i++) {
-        var l=arr[i];
-        if (l.style.display === 'none') {
-          continue;
-        }
-        if (l.href.charAt(l.href.length-1) === '#') {
-          l.dispatchEvent(customEvent);
-          if ('%s' === 'True') {
-            break;
-          }
-        }
-     }
-     """ % incremental)
-
-
-# Hooks and Patches
 gui_hooks.reviewer_did_show_question.append(run_command_field)
+# Hooks and Patches
 if ankiversion.startswith("2.0"): # 2.0.x
     Reviewer._keyHandler = wrap(
         Reviewer._keyHandler, _newKeyHandler, "around")
 else: # 2.1.x
     addHook("reviewStateShortcuts", _addShortcuts)
-
